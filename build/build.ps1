81bbfda2c1c7309f8291f04f48096250bbdaed25
Shad Storhaug <shad@shadstorhaug.com>
2019-07-07 02:43:08 +0700

    Removed unnecessary CommonAssemblyInfo.cs generation, as we can pass the FileVersion in order to generate the AssemblyFileAttribute.

--- build/build.ps1
+++ build/build.ps1
@@ -175,6 +175,7 @@ task Compile -depends Clean, Init, Restore -description "This task compiles the 
 				/p:Configuration=$configuration `
 				/p:AssemblyVersion=$assemblyVersion `
 				/p:InformationalVersion=$pv `
+				/p:FileVersion=$version `
 				/p:Product=$product_name `
 				/p:Company=$company_name `
 				/p:Copyright=$copyright `
@@ -325,12 +326,6 @@ function Get-Version() {
 }
 
 function Prepare-For-Build() {
-	Backup-File $common_assembly_info 
-
-	Generate-Assembly-Info `
-		-version $version `
-		-file $common_assembly_info
-
 	Update-Constants-Version $packageVersion
 
 	if ($generateBuildBat -eq $true) {
@@ -346,40 +341,6 @@ function Update-Constants-Version([string]$version) {
 	(Get-Content $constantsFile) | % {
 		$_-replace "(?<=LUCENE_VERSION\s*?=\s*?"")([^""]*)", $version
 	} | Set-Content $constantsFile -Force
-}
-
-function Generate-Assembly-Info {
-param(
-	[string]$version,
-	[string]$file = $(throw "file is a required parameter.")
-)
-
-  $asmInfo = "/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the ""License""); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an ""AS IS"" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-using System;
-using System.Reflection;
-
-[assembly: AssemblyFileVersion(""$version"")]
-"
-	$dir = [System.IO.Path]::GetDirectoryName($file)
-	Ensure-Directory-Exists $dir
-
-	Write-Host "Generating assembly info file: $file"
-	Out-File -filePath $file -encoding UTF8 -inputObject $asmInfo
 }
 
 function Generate-Build-Bat {
