73f1e5e904f229995a462fe98d618fbb59bd99d7
Shad Storhaug <shad@shadstorhaug.com>
2020-01-11 02:28:31 +0700

    Added naming exclusion for _Scratch class (which only exists to make a J2N namespace real for now)

--- src/Lucene.Net/Support/_Scratch.cs
+++ src/Lucene.Net/Support/_Scratch.cs
@@ -1,9 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Lucene.Net.Support;
 
 namespace J2N.Collections.Generic.Extensions
 {
+    /// <summary>
+    /// This only exists to ensure we have a resolvable J2N.Collections.Generic.Extensions
+    /// namespace until J2N is updated to include it
+    /// </summary>
+    [ExceptionToClassNameConvention]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>")]
     internal class _Scratch
     {
     }
